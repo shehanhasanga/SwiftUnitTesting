c89dbb94eb1613dc41542dd4b76c8aff2d7e4c0f
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-18 17:27:20 +0530

    Initial Commit

---
+++ Unittest/UnittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnittestApp.swift
+//  Unittest
+//
+//  Created by shehan karunarathna on 2022-02-18.
+//
+
+import SwiftUI
+
+@main
+struct UnittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
